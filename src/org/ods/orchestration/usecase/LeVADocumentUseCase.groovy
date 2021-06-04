25633103e81d0fb57392e24f1fa9a73dcd2eb5fc
zxBCN Farre_Basurte,Juan_Antonio (IT EDS) EXTERNAL <juan_antonio.farre_basurte.ext@boehringer-ingelheim.com>
2021-06-04 16:44:53 +0200

    Debug

--- src/org/ods/orchestration/usecase/LeVADocumentUseCase.groovy
+++ src/org/ods/orchestration/usecase/LeVADocumentUseCase.groovy
@@ -1702,14 +1702,16 @@ class LeVADocumentUseCase extends DocGenUseCase {
     }
 
     // This is a cache for the method getLatestDocVersionId. Do not use outside of that method.
-    private volatile docVersions = new ConcurrentHashMap<String, Long>()
+    private volatile docVersions = Collections.<String, Long>synchronizedMap([:])
 
     private Long getLatestDocVersion(List<Map> trackingIssues) {
         def versionList = trackingIssues.collect { issue ->
             ServiceRegistry.instance.get(Logger).info("Blirp: ${issue.key}")
-            def version = docVersions.computeIfAbsent((String) issue.key, { String key ->
-                jiraUseCase.getDocVersionId(issue)
-            })
+            def version = docVersions[issue.key]
+            if (version == null) {
+                version = this.jiraUseCase.getDocVersionId(issue)
+                docVersions[issue.key] = version
+            }
             return version
         }
 
